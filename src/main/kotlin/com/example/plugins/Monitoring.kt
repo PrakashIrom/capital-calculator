0d8faa684d73ca6c806b34e9c09c07467f482510
Shuqian Hon <honsq90@gmail.com>
2024-10-01 06:28:57 +1000

    Add sample Ktor project

---
+++ src/main/kotlin/com/example/plugins/Monitoring.kt
@@ -0,0 +1,23 @@
+package com.example.plugins
+
+import io.ktor.server.application.*
+import io.ktor.server.metrics.micrometer.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import io.micrometer.prometheus.*
+
+fun Application.configureMonitoring() {
+    val appMicrometerRegistry = PrometheusMeterRegistry(PrometheusConfig.DEFAULT)
+    
+    install(MicrometerMetrics) {
+        registry = appMicrometerRegistry
+
+    }
+
+
+    routing {
+        get("/observability/metrics") {
+            call.respond(appMicrometerRegistry.scrape())
+        }
+    }
+}
